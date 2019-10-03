82f46c5145d21fdfe632319b484024e1ed1c47c9
lfryemason <lfryemason@unm.edu>
2019-10-02 20:21:43 -0600

    Updates

---
+++ precache-manifest.23fdd52835b865b85ed6ae772c117370.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1e531e3125f6282d73c3e62ea8eabd4",
+    "url": "/bogle-calc/index.html"
+  },
+  {
+    "revision": "899aebc2fb44bdb6fa64",
+    "url": "/bogle-calc/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "e007c3fddaa0989fafcf",
+    "url": "/bogle-calc/static/js/2.d4b94315.chunk.js"
+  },
+  {
+    "revision": "899aebc2fb44bdb6fa64",
+    "url": "/bogle-calc/static/js/main.d67e7517.chunk.js"
+  },
+  {
+    "revision": "9297e839cb757a1b3b22",
+    "url": "/bogle-calc/static/js/runtime-main.5e0b595f.js"
+  },
+  {
+    "revision": "12b75d5552814cd619a142009b215958",
+    "url": "/bogle-calc/static/media/CardTop.12b75d55.png"
+  },
+  {
+    "revision": "b8beb4d787c715a8a7dfaca9c5eed330",
+    "url": "/bogle-calc/static/media/DaybreakCoronet.b8beb4d7.png"
+  },
+  {
+    "revision": "72ff5acd9193d227575c5c14c31f8878",
+    "url": "/bogle-calc/static/media/DryadArbor.72ff5acd.png"
+  },
+  {
+    "revision": "bfdb8f3153fdb2af9075034292e438cd",
+    "url": "/bogle-calc/static/media/EtherealArmor.bfdb8f31.png"
+  },
+  {
+    "revision": "9fde1c5b9ed7ea3854657361f6067182",
+    "url": "/bogle-calc/static/media/GladecoverScout.9fde1c5b.png"
+  },
+  {
+    "revision": "189d96ad9ddb905005a3f394a6ee704b",
+    "url": "/bogle-calc/static/media/GryffsBoon.189d96ad.png"
+  },
+  {
+    "revision": "7b4082072e501a2e28340413c85c0830",
+    "url": "/bogle-calc/static/media/HyenaUmbra.7b408207.png"
+  },
+  {
+    "revision": "f6f4c7853236c324a0a570bca3aa2704",
+    "url": "/bogle-calc/static/media/KorSpiritdancer.f6f4c785.png"
+  },
+  {
+    "revision": "f9aecbfbf7dcbed10d15f8483e2360a7",
+    "url": "/bogle-calc/static/media/Rancor.f9aecbfb.png"
+  },
+  {
+    "revision": "6315ae8ecafe1baf04a282297e975d3c",
+    "url": "/bogle-calc/static/media/SlipperyBogle.6315ae8e.png"
+  },
+  {
+    "revision": "9ee531047e791d404a3a0059561d5d4c",
+    "url": "/bogle-calc/static/media/SpiderUmbra.9ee53104.png"
+  },
+  {
+    "revision": "d3d4df3aff765879b886d56502ac8e6f",
+    "url": "/bogle-calc/static/media/SpiritLink.d3d4df3a.png"
+  },
+  {
+    "revision": "46b75a2c7d03587de4891c06b924ff4c",
+    "url": "/bogle-calc/static/media/SpiritMantle.46b75a2c.png"
+  },
+  {
+    "revision": "b397338db9696be8a0b79777d0978145",
+    "url": "/bogle-calc/static/media/UnflinchingCourage.b397338d.png"
+  }
+]);
